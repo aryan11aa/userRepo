65a1c8dfaa09f561f9891502a8422ecbe6fb340b
Aryan Rohit Adsul <aryanadsul29@gmail.com>
2024-11-25 13:00:28 +0530

    even file added

---
+++ even.java
@@ -0,0 +1,13 @@
+public class even {
+  
+  
+    public static void main(String[] args) {
+        int num;
+        if(num%2==0)
+    {
+        System.out.println("no is even ");
+    }else{
+        System.out.println("Number is odd");
+    }
+    }
+}
