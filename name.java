41158774c2d725fe850477969b48b5575767d515
Aryan Rohit Adsul <aryanadsul29@gmail.com>
2024-11-25 12:53:56 +0530

    changes on name file saved

--- name.java
+++ name.java
@@ -1,7 +1,9 @@
 public class name {
 
     public static void main(String[] args) {
+        int id=1;
         String name="Aryan";
-        System.out.println("USer name is "+name);
+        System.out.println("User name is "+name);
+        System.out.println("user id is "+id);
     }
 }
